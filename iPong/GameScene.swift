f9072bd0e99d143cfdf0c8be5abc81b73ecffd70
Fe1ky <76542408+Fe1ky@users.noreply.github.com>
2022-01-24 17:38:12 -0600

    Added Scoring and starts the ball in the center when a point is scored

--- iPong/GameScene.swift
+++ iPong/GameScene.swift
@@ -14,8 +14,18 @@ class GameScene: SKScene {
     var playerone = SKSpriteNode()
     var playertwo = SKSpriteNode()
     
+    var playerOneScore = SKLabelNode()
+    var playerTwoScore = SKLabelNode()
+    
+    var score = [Int]()
     
     override func didMove(to view: SKView) {
+        
+        startGame()
+        
+        playerOneScore = self.childNode(withName: "playerOneScore") as! SKLabelNode
+        playerTwoScore = self.childNode(withName: "playerTwoScore") as! SKLabelNode
+
         
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         playerone = self.childNode(withName: "playerone") as! SKSpriteNode
@@ -33,6 +43,32 @@ class GameScene: SKScene {
         
     }
     
+    func addScore(playerWhoWon : SKSpriteNode){
+        
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if playerWhoWon == playerone{
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        }
+        else if playerWhoWon == playertwo {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        }
+        playerTwoScore.text = "\(score[1])"
+        playerOneScore.text = "\(score[0])"
+    }
+    
+    func startGame(){
+        score = [0,0]
+        
+        playerTwoScore.text = "\(score[1])"
+        playerOneScore.text = "\(score[0])"
+
+        
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         for touch in touches{
@@ -46,13 +82,22 @@ class GameScene: SKScene {
         for touch in touches{
             let location = touch.location(in: self)
             
-            playerone.run(SKAction.moveTo(x: location.x , duration: 0.2))
+            playerone.run(SKAction.moveTo(x: location.x , duration: 0.0))
         }
     }
 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
-        playertwo.run(SKAction.moveTo(x: ball.position.x , duration: 1.0))
+        playertwo.run(SKAction.moveTo(x: ball.position.x , duration: 0.5))
+        
+        if ball.position.y <= playerone.position.y + 40 {
+            addScore(playerWhoWon: playertwo)
+        }
+        else if ball.position.y >= playertwo.position.y - 40{
+            addScore(playerWhoWon: playerone)
+        }
+        
+        
     }
 }
